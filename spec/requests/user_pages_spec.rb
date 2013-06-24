4a7c7b406b92cb3cd3836b8c20ea48ae748cd707
marvin <kokororok_15@yahoo.com>
2013-06-24 10:58:59 +0800

    add user microposts

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,12 +3,28 @@ require 'spec_helper'
 describe "UserPages" do
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user)}
+    let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo")}
+    let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar")}
+    
+    before {visit user_path(user)}
+
+    it { should have_selector('h1', text: user.name)}
+    it { should have_selector('title', text: user.name)}
+
+    describe "microposts" do
+      it { should have_content(m1.content)}
+      it { should have_content(m2.conten)}
+      it { should have_content(user.microposts.count)}
+    end
+  end
   describe "index" do
 
     let(:user) {FactoryGirl.create(:user)}
 
     before(:each) do
-      valid_signin user
+      valid sign_in user
       visit users_path
   
       it { should have_selector('title', text: 'All users')}
