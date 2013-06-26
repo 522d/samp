b7c38d57e0848c8dbb99c6f4570db159d1900515
marvin <kokororok_15@yahoo.com>
2013-06-26 17:12:54 +0800

    fixed error on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates(:password, presence: true, length: {minimum: 6})
   validates(:password_confirmation , presence: true)
-  after_validation { self.error.messages.delete(:password_digest)}
 
   def feed
     Micropost.from_users_followed_by(self)
